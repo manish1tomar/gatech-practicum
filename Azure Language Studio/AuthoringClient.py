24e50ab54e4f1931bde886c56e3ca667e78ef944
manish1tomar <52719316+manish1tomar@users.noreply.github.com>
2025-02-01 15:33:57 -0600

    Update AuthoringClient.py

--- Azure Language Studio/AuthoringClient.py
+++ Azure Language Studio/AuthoringClient.py
@@ -2,6 +2,6 @@ from azure.core.credentials import AzureKeyCredential
 from azure.ai.language.questionanswering.authoring import AuthoringClient
 
 endpoint = "https://language-gatech.cognitiveservices.azure.com/language/:query-knowledgebases?projectName=Project-gatech&api-version=2021-10-01&deploymentName=production"
-credential = AzureKeyCredential("BXWL0nYXpDhyohFV5MeZZf91tlSyHb3J34offV7UsSC5N3BX4OVIJQQJ99BAACYeBjFXJ3w3AAAaACOGs4uk")
+credential = AzureKeyCredential("")
 
 client = AuthoringClient(endpoint, credential)
