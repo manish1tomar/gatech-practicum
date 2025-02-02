90a5d5e2f2c25f35cd74cf5b4e58146864563dc8
ruchy1004 <ruchy.chaudhary@gmail.com>
2025-02-01 20:14:17 -0600

    working answer

--- Azure Language Studio/QuestionAnsweringClient.py
+++ Azure Language Studio/QuestionAnsweringClient.py
@@ -1,17 +1,24 @@
 from azure.core.credentials import AzureKeyCredential
-from azure.ai.language.questionanswering import QuestionAnsweringClient
+from azure.ai.language.questionanswering.aio import QuestionAnsweringClient
+import asyncio
 
 endpoint = "https://eastus.api.cognitive.microsoft.com/"
 credential = ""
-knowledge_base_project = "Project-gatech"
+knowledge_base_project = "abc"
 api_version="2021-10-01"
 deploymentName="production"
 
-client = QuestionAnsweringClient(endpoint, credential)
+client = QuestionAnsweringClient(endpoint, AzureKeyCredential(credential))
 
-output = client.get_answers(question="Hi", project_name=knowledge_base_project, deployment_name=deploymentName, api_version=api_version)
+output = asyncio.run( client.get_answers(question="Hi"
+                            , project_name=knowledge_base_project
+                            , deployment_name=deploymentName)
+             )
 
-for candidate in output.answers:
+'''for candidate in output.answers:
     print("({}) {}".format(candidate.confidence, candidate.answer))
     print("Source: {}".format(candidate.source))
+'''
 print("A: {}".format(output.answers[0].answer))
+
+asyncio.run(client.close())
